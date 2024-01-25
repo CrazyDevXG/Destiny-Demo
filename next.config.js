6d64b829d77dc896a8cc50be8332dd9c6ef00746
CrazyDevXG <crazydev.pro@gmail.com>
2024-01-25 20:38:02 +0700

    mi-edit1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /**@type {import('next').NextConfig}*/
 const nextConfig = {
-
+    env: {       
+        MYSQL_HOST: 'thsv75.hostatom.com',
+        MYSQL_PORT: '3306',
+        MYSQL_NAME: 'destiny_loveDB',
+        MYSQL_USER: 'crazydev_dsLove',
+        MYSQL_PASSWD: 'fwI$2735e',
+        NEXTAUTH_SECRET: 'CrazyDev_NextJS_DestinyLove',
+      },
   
 }
  
